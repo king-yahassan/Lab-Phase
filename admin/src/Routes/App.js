653c5e9413a053001a81ef44622122c9ba478ef7
king-yahassan <king.yahassane@gmail.com>
2024-04-22 13:11:12 +0000

    Triying Frontend with the admin sever: Auth is done

---
+++ admin/src/Routes/App.js
@@ -0,0 +1,20 @@
+import {createBrowserRouter} from "react-router-dom"
+import Home from "../Pages/Home"
+import Products from "../Pages/Products"
+import ProtectedRoute from "../Services/ProtectedRoute"
+import SignIn from "../Components/Connection/SignIn";
+
+
+const token = localStorage.getItem("token");
+const router = createBrowserRouter([
+    {path: "/" , element: <SignIn />} ,
+    {path: "/products" , element: <Products />},
+    {
+        path: "/home/", element:
+            <ProtectedRoute isAuthenticated={token}>
+                <Home />
+            </ProtectedRoute>
+    }
+])
+
+export default router
